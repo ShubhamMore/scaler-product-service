d4441b34f4252c5baed477ee835ea67dfd1852bd
shubhammore <shubhammore1796@gmail.com>
2025-04-01 22:29:24 +0530

    Added logs and generic exception handling. Tested exception handling by adding arithmetic exception on purpose.

--- src/main/java/com/woolf/project/product/controllers/ProductController.java
+++ src/main/java/com/woolf/project/product/controllers/ProductController.java
@@ -4,6 +4,8 @@ import com.woolf.project.product.dtos.StoreProductDTO;
 import com.woolf.project.product.models.Product;
 import com.woolf.project.product.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,9 +20,11 @@ public class ProductController {
     }
 
     @GetMapping("/")
-    public List<Product> getAllProducts(){
-        return productService.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts(){
+        // int a = 1 / 0;
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
     }
+
     @GetMapping("/{id}")
     public Product getSingleProduct(@PathVariable("id") long id){
 
--- src/main/java/com/woolf/project/product/services/StoreProductService.java
+++ src/main/java/com/woolf/project/product/services/StoreProductService.java
@@ -3,6 +3,7 @@ package com.woolf.project.product.services;
 import com.woolf.project.product.dtos.StoreProductDTO;
 import com.woolf.project.product.models.Category;
 import com.woolf.project.product.models.Product;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 @Service
+@Log4j2
 public class StoreProductService implements ProductService {
 
     private RestTemplate restTemplate;
@@ -22,24 +24,41 @@ public class StoreProductService implements ProductService {
     //ToDo : Need to implement all other 7 APIs of fake store    16 oct, 23 oct, 25 oct
     @Override
     public Product getSingleProduct(Long id) {
-        StoreProductDTO response = restTemplate.getForObject(
-                "https://fakestoreapi.com/products/"+id,
-                StoreProductDTO.class);
+        try {
+            StoreProductDTO response = restTemplate.getForObject(
+                    "https://fakestoreapi.com/products/"+id,
+                    StoreProductDTO.class);
 
-        return convertFakeStoreToProduct(response);
+            return convertFakeStoreToProduct(response);
+        } catch (Exception e) {
+            log.error("Some Exception occurred",e);
+            return null;
+        }
+
     }
 
     @Override
     public StoreProductDTO addNewProduct(StoreProductDTO product) {
-        StoreProductDTO response = restTemplate.postForObject("https://fakestoreapi.com/products",product,StoreProductDTO.class);
-        return response;
+        try {
+            StoreProductDTO response = restTemplate.postForObject("https://fakestoreapi.com/products",product,StoreProductDTO.class);
+            return response;
+        } catch (Exception e) {
+            log.error("Some Exception occurred",e);
+            return null;
+        }
+
     }
 
     @Override
     public List<Product> getAllProducts() {
-        //Todo Need add Product [].Class instead List.Class
-        List<Product> productList = restTemplate.getForObject("https://fakestoreapi.com/products", List.class);
-        return productList;
+//        try {
+//        Todo Need add Product [].Class instead List.Class
+            List<Product> productList = restTemplate.getForObject("https://fakestoreapi.com/products", List.class);
+            return productList;
+//        } catch (Exception e){
+//            log.error("Some Exception occurred",e);
+//            return null;
+//        }
     }
 
     private Product convertFakeStoreToProduct(StoreProductDTO StoreProductDTO ) {
